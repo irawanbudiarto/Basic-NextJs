4e3e70a122f95c7a91d590e2b3f8ab94b974a795
irawanbudiarto <irawanbudiarto96@gmail.com>
2021-07-03 12:34:05 +0700

    import component header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Header from "../components/Header"
 
 export default function Home() {
   return (
     <>
-      <header>
-        <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/blog"><a>Blog</a></Link></li>
-          <li><Link href="/users"><a>Users</a></Link></li>
-        </ul>
-      </header>
+      <Header />
       <h1>Welcome Irawan</h1>
     </>
   )
